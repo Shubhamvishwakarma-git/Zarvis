aff9b4c0ce8d9bccfc1d1d36a5ba413b3d4ae81d
Shubham Kumar vishwakarma <sv79132@gmail.com>
2025-02-04 00:49:26 +0530

    updating changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
 import Menu from './main/Menu'
-import IronMan from './components/Ironman'
+// import IronMan from './components/Ironman'
 
 const App = () => {
   return (
     <>
       <Sidebar/>
       <Menu/>
-      <IronMan/>
+      {/* <IronMan/> */}
       
     </>
   )
